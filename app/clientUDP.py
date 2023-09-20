d41037f6b31b98b4402e942b4b3995b815ce2c94
rrlc <ladvocatrodrigo@gmail.com>
2023-09-20 19:24:39 -0300

    regina

--- app/clientUDP.py
+++ app/clientUDP.py
@@ -1,17 +1,19 @@
 from socket import *
 import os
 from datetime import datetime
-from rdtReceiver import Receiver
-from rdtSender import Sender
 from utils import BUFFER_SIZE, IP, PORT
 from threading import Thread
+from threading import Lock
+from time import time
+
+data_lock = Lock()
 
 class Client:
     def __init__(self) -> None:
         self.response = ""
+        self.pkt_id = '1'
+        self.ack_confirm = False
         self.client_socket = socket(AF_INET, SOCK_DGRAM)
-        self.receiver = Receiver(self.client_socket)
-        self.sender = Sender(self.client_socket)
 
         self.sending_thread = Thread(target = self.sending)
         self.receiving_thread = Thread(target = self.receiving)
@@ -19,21 +21,60 @@ class Client:
         
     def sending(self):
         started = False
-        while (1):
-            self.response = input()
-            self.client_socket.sendto(self.response.encode(), (IP, PORT))
-            if (not started):
-                self.receiving_thread.daemon = True
-                self.receiving_thread.start()
-                started = True        
+        try:
+            while (1):
+                with data_lock:            
+                    self.pkt_id = '1' if (self.pkt_id == '0') else '0'
+                    self.ack_confirm = False
+                
+                self.response = input()
+                self.response = self.pkt_id + self.response
+                self.client_socket.sendto(self.response.encode(), (IP, PORT))
+                
+                if (not started):
+                    self.receiving_thread.daemon = True
+                    self.receiving_thread.start()
+                    started = True
 
-            if (self.response == "bye"): break
+                t_start = time()
+
+                if (self.response[1:] == "bye"):
+                    del(self.receiving_thread)
+                    break
+
+                while (not self.ack_confirm):
+                    t_curr = time()
+                    if (t_curr - t_start > 5):
+                        # timeout
+                        self.client_socket.sendto(self.response.encode(), (IP, PORT))
+                        t_start = time()
+                        continue
+
+                    if (self.ack_confirm): break  # recebeu o ack correto
+            
+        except Exception:
+            print("closing socket")
+        finally:
+            self.client_socket.close()
+            return
     
     def receiving(self):
-        while (1):
-            data, server_address = self.client_socket.recvfrom(1024)
-            data = data.decode()
-            print(data)
-            if (self.response == "bye"): break
-        return
+        try:
+            while (1):
+                data, server_address = self.client_socket.recvfrom(1024)
+                data = data.decode()
+                
+                # data[0] == '0' -> ack0
+                # data[0] == '1' -> ack1
+                # data[0] == '2' -> pertence a outro cliente
+
+                with data_lock:
+                    if (data[0] == self.pkt_id):
+                        self.ack_confirm = True
+
+                print(data[1:])
+            return
+        except Exception:
+            print("ate logo")
+
 novo = Client()
--- app/serverUDP.py
+++ app/serverUDP.py
@@ -1,8 +1,6 @@
 from socket import *
 import os
 from datetime import datetime
-from rdtReceiver import Receiver
-from rdtSender import Sender
 from utils import BUFFER_SIZE, IP, PORT
 import threading 
 from threading import Thread
@@ -16,9 +14,8 @@ class Server:
 
         self.server_socket = socket(AF_INET, SOCK_DGRAM)
         self.server_socket.bind(('localhost', PORT))
-        self.receiver = Receiver(self.server_socket)
-        self.sender = Sender(self.server_socket)
         self.dict_users = {} 
+        self.map_adr_pkt = {}
         self.is_waiting = True
 
         self.log_thread.start()
@@ -31,8 +28,8 @@ class Server:
     
     def hi(self, message, client_address):
         self.dict_users[client_address] = message[16:] #15
+        self.map_adr_pkt[client_address] = '0'
         return message
-    
     
     def receive(self):
         while (1):
@@ -40,32 +37,51 @@ class Server:
             data = data.decode()
             new_thread = Thread(target = self.start2, args = (client_address, data))
             new_thread.start()
-
         return
             
-
     def start2(self, target_address, data):
         try:
+            listc=False
             if (not (target_address in self.dict_users)):
                 data = self.hi(data, target_address)
             
-            elif(data == "bye"):
-                data = self.final_message(data, target_address)
+            elif(data[1:] == "bye"):
+
+                data = self.final_message(data[1:], target_address)
                 self.dict_users.pop(target_address)
+                print(self.dict_users)
+                self.map_adr_pkt.pop(target_address)
                 for users in self.dict_users:
-                    self.server_socket.sendto(data.encode(), users)
+                    self.server_socket.sendto(("0" + data).encode(), users)
 
-            elif(data == "list"):
+            elif(data[1:] == "list"):
+                string = ""
+                listc = True
+                if (data[0] == self.map_adr_pkt):
+                    self.map_adr_pkt[target_address] = '1' if (self.map_adr_pkt[target_address] == '0') else '0'
+
+                ack = data[0]
+                for user in self.dict_users:
+                    string += f'/ {self.dict_users[user]} -> {user}\n'
+                print(string)
+                string = ack + string
+                self.server_socket.sendto(string.encode(), target_address)
             
+            if (target_address in self.dict_users and not listc):
+                # arquivo duplicado
+                
+                if (self.map_adr_pkt[target_address] == data[0]):
+                    self.map_adr_pkt[target_address] = '1' if (self.map_adr_pkt[target_address] == '0') else '0'
+                
+                ack = data[0]
+                data = data[1:]
                 for user in self.dict_users:
-                    print(self.dict_users[user])
-                    self.server_socket.sendto(self.dict_users[user].encode(), target_address)
-
-            if (target_address in self.dict_users):
-                data = self.final_message(data, target_address)
-                for user in self.dict_users:
-                    self.server_socket.sendto(data.encode(), user)  
-            print(data)      
+                    data_aux = self.final_message(data, target_address)
+                    data_aux = '2' + data_aux if(user != target_address) else ack + data_aux
+                    print(user)
+                    self.server_socket.sendto(data_aux.encode(), user)  
+                print("\n")
+            #print(data)      
         except KeyboardInterrupt:
             print("bye")
             return
